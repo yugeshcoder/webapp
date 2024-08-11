9c9891d7bf3baed64d9ec2224fbf96bab22a1efa
yugeshcoder <143157324+yugeshcoder@users.noreply.github.com>
2024-08-12 03:21:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("left").onclick = function(){
+  document.getElementById("div2").style.left = `-412px`;
+};
+document.getElementById("right2").onclick = function(){
+  document.getElementById("div2").style.left = 0;
+};
