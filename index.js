05976e8fd96efdce76f583b208e29a8456c8ebb4
yugeshcoder <yugesh.v.1013@gmail.com>
2024-08-12 18:48:40 +0530

    new look

--- index.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("left").onclick = function(){
-  document.getElementById("div2").style.left = `-412px`;
-};
-document.getElementById("right2").onclick = function(){
-  document.getElementById("div2").style.left = 0;
-};
